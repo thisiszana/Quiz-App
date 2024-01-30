537040f4a2de3e0a9865e0f4b9bd67aedb5cf9c7
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-01-30 16:19:35 +0330

    Show questions to the user. Showing the answer to the question when the user clicks, whether the answer to the question is correct or incorrect. Apply 10 points for each correct question. Showing the next question by clicking the Next button and fixing the bugs in this section. Enable the option to finish the game and create the end page of the questions.

--- JS/formatedData.js
+++ JS/formatedData.js
@@ -5,16 +5,14 @@ const formatedData = question => {
         };
 
         const answer = [ ...item.incorrect_answers];
-        const correctAnswerIndex = Math.round(Math.random() * 4);
+        const correctAnswerIndex = Math.floor(Math.random() * 4);
         answer.splice(correctAnswerIndex, 0, item.correct_answer);
-
         questionObject.answer = answer;
         questionObject.correctAnswerIndex = correctAnswerIndex;
 
         return questionObject;
     });
 
-    console.log(result);
 
     return result;
 } 
--- JS/game.js
+++ JS/game.js
@@ -2,22 +2,86 @@ import formatedData from "./formatedData.js";
 
 const loader = document.getElementById("loader");
 const container = document.querySelector(".container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
+const scoreCount = document.getElementById("score-count");
+const nextBtn = document.getElementById("next-button");
+const questionCount = document.getElementById("question-count");
+const finishButton = document.getElementById("finish-button");
 
+const CORRECT_BONUS = 10;
 const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formatData = null;
+let questionIndex = 0;
+let correctAnswer = null;
+let score = 0
+let isAccepted = true;
 
 const fetchData = async () => {
     const response = await fetch(URL);
     const json = await response.json();
-    console.log(json);
     formatData = formatedData(json.results);
     start();
-}
+};
+
+const showQuestion = () => {
+    const { question, answer, correctAnswerIndex } = formatData[questionIndex];
+    questionCount.innerText = questionIndex + 1;
+    questionText.innerText = question;
+    correctAnswer = correctAnswerIndex;
+    console.log(correctAnswer);
+    answerList.forEach((answerBtn, index) => {
+        answerBtn.innerText = answer[index];
+    })
+};
 
 const start = () => {
+    showQuestion();
     loader.style.display = "none";
     container.style.display = "block";
+};
+
+const checkAnswer = (event, index) => {
+    if (!isAccepted) return;
+    isAccepted = false;
+
+    const isCorrect = index === correctAnswer ? true : false;
+
+    if (isCorrect) {
+        event.target.classList.add("correct");
+        score += CORRECT_BONUS;
+        scoreCount.innerText = score;
+    } else {
+        event.target.classList.add("incorrect");
+        answerList[correctAnswer].classList.add("correct");
+    }
+};
+
+const nextBtnHandler = () => {
+    questionIndex++;
+
+    if (questionIndex < formatData.length) {
+        isAccepted = true;
+        removeClassName();
+        showQuestion();
+    } else {
+        finishHandler();
+    }
+};
+
+const removeClassName = () => {
+    answerList.forEach(item => item.className = "answer-text");
+};
+
+const finishHandler = () => {
+    localStorage.setItem("score", JSON.stringify(score));
+    window.location.assign("/html/end.html");
 }
 
-window.addEventListener("DOMContentLoaded", fetchData)
+window.addEventListener("DOMContentLoaded", fetchData);
+nextBtn.addEventListener("click", nextBtnHandler);
+finishButton.addEventListener("click", finishHandler)
+answerList.forEach((answerBtn, index) => {
+    answerBtn.addEventListener("click", (event) => checkAnswer(event, index))
+})
